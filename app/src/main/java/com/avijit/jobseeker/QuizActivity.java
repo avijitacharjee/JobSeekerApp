f970dfe9fd3fdb684bf28888bd04dead86bd0fae
Avijit Acharjee <avijitach@gmail.com>
2019-12-02 23:30:39 +0600

    quiz activity questions

--- app/src/main/java/com/avijit/jobseeker/QuizActivity.java
+++ app/src/main/java/com/avijit/jobseeker/QuizActivity.java
@@ -1,7 +1,11 @@
 package com.avijit.jobseeker;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,6 +31,9 @@ import java.util.List;
 import java.util.Map;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Handler mainHandler = new Handler();
+
     private TextView questionTextView;
     private ListView listView;
     private QuizListAdapater adapter;
@@ -35,11 +42,14 @@ public class QuizActivity extends AppCompatActivity {
     private static JSONObject jsonObject= new JSONObject();
     private String question="";
     private String answer;
+    CountDownTimer countDownTimer;
+    static int TIME_IN_MILLIS=1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
 
         questionTextView= findViewById(R.id.question_text_view);
         questionTextView.setText("lakjsdflkjasdflkjasdfkdkfkjfjfjfjf");
@@ -49,7 +59,7 @@ public class QuizActivity extends AppCompatActivity {
 
         subCategoryId = getIntent().getExtras().getInt("subCategoryId");
         setOptionTexts();//It also initialises the jsonObject
-        try {
+       /* try {
             JSONArray data = jsonObject.getJSONArray("data");
             int length= data.length();
             for(int i=0;i<length;i++)
@@ -69,7 +79,7 @@ public class QuizActivity extends AppCompatActivity {
             System.out.println(e.toString());
         }
 
-
+*/
     }
     public void setOptionTexts()
     {
@@ -84,23 +94,31 @@ public class QuizActivity extends AppCompatActivity {
                             List<Integer> ids= new ArrayList<>();
                             JSONObject jsonObject = new JSONObject(response);
                             QuizActivity.jsonObject = jsonObject;
-
                             JSONArray data = jsonObject.getJSONArray("data");
                             int length= data.length();
-                            for(int i=0;i<length;i++)
+                            JSONObject questionsJsonObj=null;
                             {
-                                JSONObject questionsJsonObj = data.getJSONObject(i);
-                                question=questionsJsonObj.getString("question");
-                                questionTextView.setText(question);
-                                options[0]=questionsJsonObj.getString("optiona");
-                                options[1]=questionsJsonObj.getString("optionb");
-                                options[2]=questionsJsonObj.getString("optionc");
-                                options[3]=questionsJsonObj.getString("optiond");
-                                answer = questionsJsonObj.getString("answer");
-
+                                questionsJsonObj = data.getJSONObject(0);
+                                changeQuestion(questionsJsonObj);
                             }
-                            adapter = new QuizListAdapater(getApplicationContext(),options);
-                            listView.setAdapter(adapter);
+                            for(int i=1;i<length;i++)
+                            {
+                                questionsJsonObj = data.getJSONObject(i);
+                                //timer1(questionsJsonObj);
+                                try {
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException e) {
+
+                                }
+                                changeQuestion(questionsJsonObj);
+                            }
+
+                            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                                }
+                            });
                             /*JSONArray data = jsonObject.getJSONArray("data");
                             int length = data.length();
                             for(int i=0;i<length;i++)
@@ -158,5 +176,53 @@ public class QuizActivity extends AppCompatActivity {
 // Add the request to the RequestQueue.
         //getApplicationContext().addToRequestQueue(jsonObjectRequest, "headerRequest");
         queue.add(stringRequest);
+    }
+    public void changeQuestion(JSONObject questionJSONObject)
+    {
+        mainHandler.post(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        try
+        {
+            question=questionJSONObject.getString("question");
+            questionTextView.setText(question);
+            options[0]=questionJSONObject.getString("optiona");
+            options[1]=questionJSONObject.getString("optionb");
+            options[2]=questionJSONObject.getString("optionc");
+            options[3]=questionJSONObject.getString("optiond");
+            answer = questionJSONObject.getString("answer");
+            Toast.makeText(getApplicationContext(),questionJSONObject.getString("id"),Toast.LENGTH_LONG).show();
+        } catch (JSONException e) {
+            questionTextView.setText("Error");
+        }
+        adapter = new QuizListAdapater(getApplicationContext(),options);
+        listView.setAdapter(adapter);
+
+    }
+    public void timer(final JSONObject questionJSONObject)
+    {
+        countDownTimer = new CountDownTimer(QuizActivity.TIME_IN_MILLIS,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                changeQuestion(questionJSONObject);
+            }
+        }.start();
+    }
+    public void timer1(JSONObject questionJSONObject)
+    {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        changeQuestion(questionJSONObject);
     }
 }
